4c54d50f20a5e437447e0d20a6c2c9a41540f3bb
Djonyx756 <75791040+Djonyx756@users.noreply.github.com>
2023-11-30 00:19:29 +0300

    Update del_cl.sh

--- scripts/del_cl.sh
+++ scripts/del_cl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ip=$1
-
 my_variable=$(grep -n "10.10.0.${ip}" /etc/wireguard/wg0.conf | cut -d ':' -f 1)
 
 sed -i "$(($my_variable-2)),$my_variable d" /etc/wireguard/wg0.conf
-
 systemctl restart wg-quick@wg0
